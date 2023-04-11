12695dd43d959c8ef56863775c3869d8a5c0bf36
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-12 01:23:30 +0300

    CustomStringConvertible protocol added

--- SwiftBook.playground/Pages/Lesson 12 Protocols.xcplaygroundpage/Contents.swift
+++ SwiftBook.playground/Pages/Lesson 12 Protocols.xcplaygroundpage/Contents.swift
@@ -12,3 +12,27 @@ let boolean = true
 print(boolean)
 
 
+// CustomStringConvertible
+
+class Shoes {
+    
+    let color: String
+    let size: Int
+    let hasLaces: Bool
+    
+    init(color: String, size: Int, hasLaces: Bool) {
+        self.color = color
+        self.size = size
+        self.hasLaces = hasLaces
+    }
+}
+
+// MARK: - CustomStringConvertible
+extension Shoes: CustomStringConvertible {
+    var description: String {
+        "Shoes(color: \(color), size: \(size), hasLaces: \(hasLaces))"
+    }
+}
+
+let shoes = Shoes(color: "Brown", size: 41, hasLaces: true)
+print(shoes)
